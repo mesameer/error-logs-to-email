0a3ce81bcfbf94523736d6617963711d9606a76b
Sameer Singh <sameer@interbitsolutions.com>
2024-01-23 18:28:55 +0530

    Fixed

---
+++ test.js
@@ -0,0 +1,14 @@
+const errorLogsToEmail = require("./error-logs-to-email");
+
+const mailOptions = {
+  user: 'user_email',
+  pass: 'password',
+  to: 'to_user_email',
+  subject: 'Error Logs for the Last Hour',
+};
+
+const executeAfterSec = 1 * 60 * 60; // 1 Hour
+
+global.mailLogger = errorLogsToEmail.logger(executeAfterSec, mailOptions);
+
+mailLogger.error("mail logger is now available for global scope.");
